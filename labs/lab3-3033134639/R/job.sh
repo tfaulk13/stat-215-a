fac0c58691e783475dd444ea49109180be27e741
Todd Faulkenberry <todd.faulkenberry@gmail.com>
2018-10-21 20:56:39 -0700

    fixing job

--- labs/lab3-3033134639/R/job.sh
+++ labs/lab3-3033134639/R/job.sh
@@ -1,3 +1,4 @@
-# !/bin/bash
-# SBATCH -- cpus-per-task 2
+#!/bin/bash
+#SBATCH -- cpus-per-task 2
+
 R CMD BATCH --no-save parallel.R parallel.Rout
--- labs/lab3-3033134639/job.sh
+++
@@ -1,3 +0,0 @@
-# !/bin/bash
-# SBATCH -- cpus-per-task 2
-R CMD BATCH --no-save parallel.R parallel.Rout
