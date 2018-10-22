c68ab789f4d3d2a13adb924905dd1d94a98a9ef4
Todd Faulkenberry <todd.faulkenberry@gmail.com>
2018-10-21 22:35:32 -0700

    job

--- labs/lab3-3033134639/R/parallel.R
+++ labs/lab3-3033134639/R/parallel.R
@@ -1,5 +1,5 @@
-source('R/load.R')
-source('R/functions.R')
+source('~/accounts/campus/todd_faulkenberry/Documents/stat-215-a/labs/lab3-3033134639/R/load.R')
+source('~/accounts/campus/todd_faulkenberry/Documents/stat-215-a/labs/lab3-3033134639/R/functions.R')
 
 library(foreach)
 library(parallel)
@@ -7,7 +7,7 @@ library(doParallel)
 library(dplyr)
 library(readr)
 
-ling.binary <- loadRData()
+ling.binary <- loadRData(path = '~/accounts/campus/todd_faulkenberry/Documents/stat-215-a/labs/lab3-3033134639/data/')
 
 ling.data <- ling.binary %>%
   select(7:474)
